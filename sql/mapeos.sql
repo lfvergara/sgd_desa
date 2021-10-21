a62cdb8ddcc08f91719f664a62df0f9485dfbd89
Federico Vergara <hu.ce.ro@gmail.com>
2021-10-21 18:34:18 -0300

    Agregando .gitignore

--- sql/mapeos.sql
+++ sql/mapeos.sql
@@ -6,3 +6,5 @@ CREATE TABLE IF NOT EXISTS delegacion (
 
 INSERT INTO delegacion(denominacion) VALUES ('La Rioja'), ('Chilecito');
 ALTER TABLE `archivos` ADD `delegacion_id` INT NOT NULL DEFAULT '1' AFTER `codigo_barras`, ADD INDEX (`delegacion_id`);
+
+ALTER TABLE `matriculados` ADD `terminos_condiciones` INT NOT NULL DEFAULT '0' AFTER `direccion_visible_web`, ADD INDEX (`terminos_condiciones`);
